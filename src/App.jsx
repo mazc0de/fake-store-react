401259c6ddda80927c0a42cff4293500729a705d
daffa404 <dandroid36@gmail.com>
2023-12-02 21:11:33 +0700

    feat: init material tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Button } from '@material-tailwind/react';
+
 export default function App() {
-  return <h1 className="text-3xl font-bold text-red-500 underline">Hello world!</h1>;
+  return <Button>Button</Button>;
 }
