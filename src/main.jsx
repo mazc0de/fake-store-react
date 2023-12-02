415aeb19e9b96265d73dbc332f05c95d9e3dab78
daffa404 <dandroid36@gmail.com>
2023-12-03 01:43:04 +0700

    feat: auth login

--- src/main.jsx
+++ src/main.jsx
@@ -6,17 +6,23 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import { Home, Login } from './pages';
 import { Layout } from './components';
+import ProtectedRoute from './hoc/ProtectedRoute';
+import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={<Home />} />
-          </Route>
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/login" element={<Login />} />
+              <Route element={<ProtectedRoute />}>
+                <Route path="/" element={<Home />} />
+              </Route>
+            </Route>
+          </Routes>
+        </AuthProvider>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
