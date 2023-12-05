9c3383bc9823c0f88c80c09a5177f64979161ad8
daffa404 <dandroid36@gmail.com>
2023-12-05 21:47:40 +0700

    feat: clickable category

--- src/pages/ProductDetail/index.jsx
+++ src/pages/ProductDetail/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { Button, Chip, Rating } from '@material-tailwind/react';
 
 import useAxios from '../../hooks/useAxios';
@@ -72,7 +72,13 @@ const ProductDetail = () => {
             </div>
             <div className="flex flex-col justify-center gap-5">
               <div className="self-start">
-                <Chip variant="outlined" value={productDetail?.category} className="rounded-full" />
+                <Link to={`/products/categories/${productDetail.category}`}>
+                  <Chip
+                    variant="outlined"
+                    value={productDetail?.category}
+                    className="rounded-full font-poppins transition-all duration-200 hover:bg-primary hover:text-white"
+                  />
+                </Link>
               </div>
               <p className="text-2xl font-semibold">{productDetail?.title}</p>
               <div className="flex items-center gap-2">
@@ -94,34 +100,6 @@ const ProductDetail = () => {
               </div>
             </div>
           </div>
-          {/* <div className="flex max-h-full flex-col gap-2 font-poppins lg:flex-row">
-            <div className="grid grid-cols-1 place-items-center rounded-lg p-5 lg:w-1/2">
-              <div className="flex w-full justify-center">
-                <img
-                  src={productDetail?.image}
-                  alt={productDetail?.name}
-                  className="w-36 rounded-lg md:w-40 lg:w-full"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col gap-2 lg:w-1/2">
-              <p className="text-xl font-semibold">{productDetail?.title}</p>
-              <div className="flex items-center gap-2">
-                <Rating value={Math.floor(productDetail?.rating?.rate)} readonly />
-                <p>({productDetail?.rating?.count})</p>
-              </div>
-              <p className="text-lg font-semibold">${productDetail?.price}</p>
-              <p>{productDetail?.description}</p>
-              <Button
-                onClick={() => addToCart(productDetail)}
-                ripple={false}
-                fullWidth={true}
-                className="bg-blue-gray-900/10 font-poppins text-blue-gray-900 shadow-none hover:scale-105 hover:bg-red-200 hover:text-red-800 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
-              >
-                Add to Cart
-              </Button>
-            </div>
-          </div> */}
         </>
       )}
     </>
