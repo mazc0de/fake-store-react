aa64d292ce76572f598821ccb726a3142ccad754
mazc0de <dandroid36@gmail.com>
2023-12-07 10:50:05 +0700

    feat: user pic

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,9 +1,10 @@
 import Layout from './Layout';
 import Header from './Header';
 import Footer from './Footer';
+import ProfileMenu from './ProfileMenu';
 import CardProduct from './CardProduct';
 import SectionTitle from './SectionTitle';
 import LoadingSpinner from './LoadingSpinner';
 import BreadcrumbsComponent from './BreadcrumbsComponent';
 
-export { Layout, Header, Footer, CardProduct, SectionTitle, LoadingSpinner, BreadcrumbsComponent };
+export { Layout, Header, Footer, ProfileMenu, CardProduct, SectionTitle, LoadingSpinner, BreadcrumbsComponent };
